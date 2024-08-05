bcc6e8c341817c6ff7bb793ae24e8258b2fb636f
Muhammad Aoun Zaidi <zaoun123@gmail.com>
2024-08-05 18:02:11 +0500

    Add user moded

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import uuid
+import os
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None, **extrafield):
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email),**extrafield)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user    
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects=UserManager()
+    USERNAME_FIELD = 'email'
+
