f7238f4bc9dcee21a4a8ff785cbb04d0574aef30
Muhammad Aoun Zaidi <zaoun123@gmail.com>
2024-08-07 15:09:41 +0500

    Add Token authentication for the user

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
-from user.serializer import UserSerializer
-
+from user.serializer import UserSerializer,AuthTokenSerializers
+from rest_framework.authtoken.views import ObtainAuthToken
 # Create your views here.
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class=UserSerializer
+    
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class=AuthTokenSerializers
